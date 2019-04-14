86e1a22f6d50bfb36c2c3f751ff4f90ef56dd977
Luc Huynh <37423125+s3724706@users.noreply.github.com>
2019-04-14 14:27:52 +1000

    Add files via upload

---
+++ Software Eng Assignment/src/Player.java
@@ -0,0 +1,32 @@
+
+public class Player {
+
+    public String Knight;
+    public String Rook;
+    public String Bishop;
+    
+    public void setName(String Knight) {
+        this.Knight = Knight;
+    }
+
+    public String getKnight() {
+        return Knight;
+    }
+    
+    public void setRook(String Rook) {
+    	this.Rook = Rook;
+    }
+    
+    public String getRook() {
+    	return Rook;
+    }
+    
+    public void setBishop(String Bishop) {
+    	this.Bishop = Bishop;
+    }
+    
+    public String getBishop() {
+    	return Bishop;
+    }
+    
+}
