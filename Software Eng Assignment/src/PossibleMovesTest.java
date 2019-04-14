d4f9ee5c27496288a7690e47f5146dd3d05a3d9c
Thien Nguyen <41944095+s3714217@users.noreply.github.com>
2019-04-14 23:51:53 +1000

    Update PossibleMovesTest.java

--- Software Eng Assignment/src/PossibleMovesTest.java
+++ Software Eng Assignment/src/PossibleMovesTest.java
@@ -69,7 +69,6 @@ class PossibleMovesTest
 	   // test with Rook
 	   test.getSpecPiece(0, 5).move(0, 4, test);
 	   test.getSpecPiece(0,2).move(0, 4, test);
-	   test.getSpecPiece(0, 
 	   
 	   Assert.assertEquals(test.getSpecPiece(0, 4).getTeam()== test.getPlayer1().getColour()));
 	   Assert.assertEquals(test.getPlayer1().getScore() == 5);
